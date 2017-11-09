f97a30d458494f7731cffd5f5002e9ffb68bd81a
Tom Schultz <tomschul@amazon.com>
2017-11-09 07:06:43 +0000

    Working.

--- cat_facts.py
+++ cat_facts.py
@@ -1,19 +1,26 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 import json
 import os
+import uuid
 
 table = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
 
 def get(event, context):
+    random_uuid = uuid.uuid4()
+
     try:
-        result = table.scan(ExclusiveStartKey={'id': '00000'}, Limit=1)
+        result = table.query(Limit=1, KeyConditionExpression=Key('sortId').gte(str(random_uuid)) & Key('id').eq('cat-fact'))
+
+        if len(result['Items']) == 0:
+            result = table.query(Limit=1, KeyConditionExpression=Key('sortId').lt(str(random_uuid)) & Key('id').eq('cat-fact'))
 
         if len(result['Items']) == 0:
             raise KeyError
     except KeyError:
         raise Exception('Couldn\'t get a random item.')
 
-    response = {'data': {'cat-fact': result[0]['cat-fact']}}
+    response = {'cat-fact': next(iter(result['Items']))['cat-fact']}
 
     return {
         'statusCode': 200,
